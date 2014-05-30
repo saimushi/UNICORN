a17112d55c0cf3ad69a63e6268b1e4c738b1a2a3
saimushi <saimushi@192.168.1.202>
2014-05-30 15:12:50 +0900

    セッションインターフェースのdocを整理

--- src/lib/FrameworkPackage/class/Session/SessionIO.interface.php
+++ src/lib/FrameworkPackage/class/Session/SessionIO.interface.php
@@ -1,8 +1,7 @@
 <?php
 
 /**
- * 独自Session
- * PHP標準のPHPSESSIDは使わず、tokenをCookieに書き込む方式によるSessionの実現の為のクラス
+ * Sessionクラスのインターフェース定義
  * @author saimushi
  */
 Interface SessionIO {
@@ -13,14 +12,32 @@ Interface SessionIO {
 	 */
 	public static function start($argDomain=NULL, $argExpiredtime=NULL, $argDSN=NULL);
 
+	/**
+	 * セッションにしまわれているデータの数を返す
+	*/
 	public static function count();
 
+	/**
+	 * セッションにしまわれているデータのキーの一覧を返す
+	*/
 	public static function keys();
 
+	/**
+	 * 指定されたキー名のセッションデータを返す
+	 * @param string キー名
+	*/
 	public static function get($argKey = NULL);
 
+	/**
+	 * セッションデータに指定されたキー名で指定された値を格納する
+	 * @param string キー名
+	 * @param mixed 値
+	*/
 	public static function set($argKey, $argment);
 
+	/**
+	 * 不要になっているハズのセッションを全てクリーンする
+	*/
 	public static function clean();
 }
 
