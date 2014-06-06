c70b1e71dcb048d61ad34fcc0709610be93f8ba2
saimushi <saimushi@192.168.101.52>
2014-06-06 17:19:20 +0900

    GRANT文をまさかの間違えていた・・・orz

--- src/lib/FrameworkManager/core/createdb.sql
+++ src/lib/FrameworkManager/core/createdb.sql
@@ -1,3 +1,3 @@
 -- MySQL --
 CREATE DATABASE IF NOT EXISTS `fwm` CHARACTER SET utf8 COLLATE utf8_general_ci;
-GRANT ALL ON `fwm`.* TO 'fwm'@'localhost' INDENTIFIED BY 'fwmpass'; 
+GRANT ALL ON `fwm`.* TO 'fwm'@'localhost' IDENTIFIED BY 'fwmpass'; 
